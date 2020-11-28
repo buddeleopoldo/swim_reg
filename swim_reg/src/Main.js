c2bc89c1eec7f2b6ab0f56dbe04e577dc7b9887f
polibudde <polibudde@gmail.com>
2020-11-28 07:05:39 -0300

    Min.js comments

--- swim_reg/src/Main.js
+++ swim_reg/src/Main.js
@@ -1,3 +1,10 @@
+// ============================= Main.js =================================//
+// =======================================================================//
+
+
+// Libraries -------------------------------------------------------------//
+// -----------------------------------------------------------------------//
+
 import * as React from 'react';
 import { View, StyleSheet, AsyncStorage} from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
@@ -10,13 +17,11 @@ import RecordsScreen from './screens/Records';
 import AccountScreen from './screens/Account';
 
 
-/*const getCurrentDate=()=>{
+// Functions and definitions ---------------------------------------------//
+// -----------------------------------------------------------------------//
 
-    var date = new Date().getDate();
-    var month = new Date().getMonth() + 1;
-    var year = new Date().getFullYear();
-    return date + '-' + month + '-' + year;
-}*/
+// Date form -----------------------------------------------------//
+
 const getFormDate=(formDate)=>{
 
     var date = formDate.getDate();
@@ -25,10 +30,26 @@ const getFormDate=(formDate)=>{
     return date + '-' + month + '-' + year;
 }
 
+/*const getCurrentDate=()=>{
+
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+    return date + '-' + month + '-' + year;
+}*/
+
+
+// Bottom tab navigator ------------------------------------------//
+
 const Tab = createBottomTabNavigator();
+
+
+// Main class ------------------------------------------------------------//
+// -----------------------------------------------------------------------//
 
 class Main extends React.Component {
 
+    // Constructor method ----------------------------------------//
     constructor(props) {
         super(props)
         this.state = {
@@ -39,6 +60,7 @@ class Main extends React.Component {
         this.loadSessions()
     }  
     
+    // Add session method ----------------------------------------//
     addSession = (sessionObj) => {
         const sessions = this.state.sessions;
         const session = {
@@ -51,9 +73,10 @@ class Main extends React.Component {
         };
         sessions.push(session);
         this.setState({ sessions, session: '' });
-        this.save()
+        this.save() 
     }
 
+    // Delete session method -------------------------------------//
     onDeleteSession = (i) => {
         const sessions = this.state.sessions;
         sessions.splice(i, 1);
@@ -61,6 +84,7 @@ class Main extends React.Component {
         this.save()
     }
 
+    // Load session from context ---------------------------------//
     loadSessions = async () => {
         try {
             const sessions = await AsyncStorage.getItem('sessions');
@@ -74,6 +98,7 @@ class Main extends React.Component {
         this.save()
     }; 
 
+    // Save session in context -----------------------------------//
     save = async () => {
         try {
           AsyncStorage.setItem('sessions', JSON.stringify(this.state.sessions));
@@ -82,24 +107,26 @@ class Main extends React.Component {
         }
     };
 
+    // Button press methods --------------------------------------//
     onPress(num) {
         alert('Session ' + num + ' pressed')
       }
-
     onLongPress() {
         alert('Looooong' + num + ' pressed')
     }
     
+    // Form error methods ----------------------------------------//
     formError(errors) {
         alert(errors.join('\n'))
     }
 
 
 
-
-
+    // Render method ---------------------------------------------//
     render() {
         return (
+
+            // Context provider for all elements 
             <SessionsContext.Provider
                 value={
                 {
@@ -117,50 +144,52 @@ class Main extends React.Component {
                     save: this.save,
                 }
             }>
+                {/* Main screen */}
                 <NavigationContainer>
-                <View style={{height: 26, backgroundColor: 'white'}}></View>
-                <Tab.Navigator
-                    screenOptions={({ route }) => ({
-                    tabBarIcon: ({ focused, color, size }) => {
-                        let iconName;
+                    <View style={{height: 26, backgroundColor: 'white'}}></View>
+                    {/* Bottom tab navigator */}
+                    <Tab.Navigator
+                        screenOptions={({ route }) => ({
+                        tabBarIcon: ({ focused, color, size }) => {
+                            let iconName;
 
-                        if (route.name === 'Sessions') {
-                        iconName = focused ? 'ios-list-box' : 'ios-list-box';
-                        size = focused ? size + 4 : size;
-                        } else if (route.name === 'Stats') {
-                        iconName = focused ? 'ios-stats' : 'ios-stats'  ;
-                        size = focused ? size + 4 : size;
-                        } else if (route.name === 'Records') {
-                        iconName = focused ? 'md-trophy' : 'md-trophy';
-                        size = focused ? size + 4 : size;
-                        } else if (route.name === 'Account') {
-                        iconName = focused ? 'md-person' : 'md-person';
-                        size = focused ? size + 4 : size;
-                        }
+                            if (route.name === 'Sessions') {
+                            iconName = focused ? 'ios-list-box' : 'ios-list-box';
+                            size = focused ? size + 4 : size;
+                            } else if (route.name === 'Stats') {
+                            iconName = focused ? 'ios-stats' : 'ios-stats'  ;
+                            size = focused ? size + 4 : size;
+                            } else if (route.name === 'Records') {
+                            iconName = focused ? 'md-trophy' : 'md-trophy';
+                            size = focused ? size + 4 : size;
+                            } else if (route.name === 'Account') {
+                            iconName = focused ? 'md-person' : 'md-person';
+                            size = focused ? size + 4 : size;
+                            }
 
-                        return <Ionicons name={iconName} size={size} color={color} />;
-                    },
-                    })}
-                    tabBarOptions={{
-                    activeTintColor: 'dodgerblue',
-                    inactiveTintColor: 'gray',
-                    labelStyle: {
-                        fontSize: 13,
-                        margin: 0,
-                        padding: 5
-                    },
-                    style: {
-                        backgroundColor: 'white',
-                        height: 60
-                    },
-                    
-                    }}
-                >
-                    <Tab.Screen name="Sessions" component={SessionsScreen} />
-                    <Tab.Screen name="Stats" component={StatsScreen} />
-                    <Tab.Screen name="Records" component={RecordsScreen} />
-                    <Tab.Screen name="Account" component={AccountScreen} />
-                </Tab.Navigator>
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                        })}
+                        tabBarOptions={{
+                        activeTintColor: 'dodgerblue',
+                        inactiveTintColor: 'gray',
+                        labelStyle: {
+                            fontSize: 13,
+                            margin: 0,
+                            padding: 5
+                        },
+                        style: {
+                            backgroundColor: 'white',
+                            height: 60
+                        },
+                        
+                        }}
+                    >
+                        <Tab.Screen name="Sessions" component={SessionsScreen} />
+                        <Tab.Screen name="Stats" component={StatsScreen} />
+                        <Tab.Screen name="Records" component={RecordsScreen} />
+                        <Tab.Screen name="Account" component={AccountScreen} />
+                    </Tab.Navigator>
                 </NavigationContainer>
             </SessionsContext.Provider>
         );
@@ -169,6 +198,9 @@ class Main extends React.Component {
 
 export default Main;
 
+
+// Main screen general styles --------------------------------------------//
+// -----------------------------------------------------------------------//
 
 const styles = StyleSheet.create({
     buttoncontainer: {
